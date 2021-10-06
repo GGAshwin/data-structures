f7d9a3ccff777cec94bc45882687ee0034d42448
Aniketh B R <88420285+anikethbr@users.noreply.github.com>
2021-10-06 10:41:22 +0530

    Fixed-Array-insertion.c

--- arrayInsertion/Ainsertion.c
+++ arrayInsertion/Ainsertion.c
@@ -1,12 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int arr[10]={1,2,6,7,9},i,pos=2,ele=100;
-    for(i=4; i >=pos;i--)
-    arr[i+1] = arr[i];
-    arr[pos] = ele;
-    for(i=0;i<6;i++)
-    printf("%d",arr[i]);
-
+    int n,pos,ele;
+    printf("Enter Number of Elements:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter Array Elements:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter Position of element to be inserted:\n");
+    scanf("%d",&pos);
+    if(pos>n || pos<0){
+        printf("Enter Valid position...!!!!!\n");
+    }
+    else{
+        printf("Enter element to be inserted:\n");
+        scanf("%d",&ele);
+        for(int i=pos;i<n;i++){
+            arr[i+1]=arr[i];
+        }
+        arr[pos] = ele;
+        for(int i=0;i<n;i++){
+            printf("%d ",arr[i]);
+        }
+    }
     return 0;
 }
